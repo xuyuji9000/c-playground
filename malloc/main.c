39a5fd46c5016dff713bb2909b20ee10219361de
Karl Xu <karl.yogi.xu@gmail.com>
2020-02-02 23:25:02 +0800

    use malloc and free

---
+++ malloc/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int* ptr;
+    int n=10;
+
+    ptr = (int*)malloc(n * sizeof(int)); 
+
+    if (ptr == NULL) { 
+        printf("Memory not allocated.\n"); 
+        exit(0); 
+    } else {
+        for(int i=0; i<n; i++) {
+            ptr[i] = i+1;
+        }
+
+        for(int i=0; i<n; ++i) {
+            printf("%d\n", ptr[i]);
+        }
+    }
+
+    free(ptr);
+    
+
+    return 0;
+}
