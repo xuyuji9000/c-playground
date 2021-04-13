61121f2da475cd2b8744fc2e91f094b5e27e32c9
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-13 20:31:38 +0800

    add a if condition example

---
+++ if-condition-with-number/README.md
@@ -0,0 +1,8 @@
+This folder contains an example about using number ( -1, 0, 1) for if condition.
+
+- Build command
+
+``` shell
+gcc -o main ./main.c
+```
+
---
+++ if-condition-with-number/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    if(-1)
+    {
+        printf("condition: -1\n");
+    }
+
+    if(0)
+    {
+        printf("condition: 0\n");
+    }
+
+    if(1)
+    {
+        printf("condition: 1\n");
+    }
+
+
+    return 0;
+}
+
